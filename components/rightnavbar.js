b31c63188c396a02b6ca25b169db3def0c40e282
jlucascaraujo <jlucascaraujo@gmail.com>
2026-04-19 11:37:55 -0300

    Refactor navigation components and enhance layout with new HTML structure

--- components/rightnavbar.js
+++ components/rightnavbar.js
@@ -1,21 +1,26 @@
 class RightNavBar extends HTMLElement {
     constructor() {
         super();
+        this.attachShadow({ mode: 'open' });
+    }
+    connectedCallback() {
+        this.loadContentFromFile('rightNavBar.html');
     }
 
-    connectedCallback(){
-        this.innerHTML = `
-        <nav>
-            <h1>Navegação</h1>
-            <h2>Sobre mim</h2>
-                <ul>
-                    <li><a href="">review do mês</a></li>
-                    <li><a href="">recomendações</a></li>
-                    <li><a href="">contantos</a></li>
-                </ul>
-        </nav>
-        `
+    async loadContentFromFile(components) {
+        try {
+            const response = await fetch(components);
+            if(!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const htmlText = await response.text();
+            this.shadowRoot.innerHTML = htmlText;
+        } catch(error) {
+            console.error('Erro ao carregar o arquivo HTML externo:', error);
+            this.shadowRoot.innerHTML = `<p>Erro carregando conteúdo: ${error.message}</p>`;
+        }
     }
+
 }
 
 customElements.define('right-nav-bar', RightNavBar);
